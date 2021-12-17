4e35041aeb694c8322e0825a8a0a39533fd3142b
blackanger <blackanger.z@gmail.com>
2021-12-17 23:27:22 +0800

    add 20211217

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -3,22 +3,26 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` for hints :)
 
-// I AM NOT DONE
+// 
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&mut vec0);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
-    vec1.push(88);
+    // vec1.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    vec.push(22);
+// 思考： 将参数改为可变引用？
+// 新手： `mut vec`  vs  `&mut Vec`
+// fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
+    // let mut vec = vec;
+    vec.push(22); // Vec::push(&mut Vec<i32>)
     vec.push(44);
     vec.push(66);
 
--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -5,11 +5,36 @@
 
 // I AM NOT DONE
 
+// ｜ 100 ｜ ｜ ｜y ｜ z｜ 
+//     ^        |    | &mut x
+//     |________|____|
+
+
+// 思考： 为神马改完顺序就可以了？
+// 思考： reborrowing ？ 后面再说
 fn main() {
-    let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
-    *z += 1000;
+    // x 是 owned type
+    let mut x = 100i32; // 4字节 安全的在栈上存储的类型，简单值类型
+
+    // 借用检查器 只检查 借用/引用 &mut x ，为了防止悬垂指针或其他指针类问题
+    let y     = &mut x;  // 'y lifetime start
+    *y += 100;  // 'y lifetime end
+
+    let z = &mut x; // 'z lifetime start
+    *z += 1000; // x == 200 + 1000 // // 'z lifetime end
+
     assert_eq!(x, 1200);
 }
+
+// Move vs Copy
+// 思考： 为什么 x 不等于 1200？
+// 为什么有了 move 还需要 Copy ?
+// fn main() {
+//     let mut x = 100i32; // 4字节 安全的在栈上存储的类型，简单值类型 
+//     // x 实现了 Copy trait (按位复制)
+//     let mut y = x; // x 被 copy 给了y
+//     let mut z = x;
+//     y += 100i32; 
+//     z += 1000;
+//     assert_eq!(x, 1200); // x == 100
+// }
