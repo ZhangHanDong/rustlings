f88b05d7512c9523a3c11e20b5ad40da017b1492
blackanger <blackanger.z@gmail.com>
2021-12-20 23:50:04 +0800

    add 20211220

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,13 +1,33 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
+//
 
 struct ColorClassicStruct {
     // TODO: Something goes here
+    name: &'static str, 
+    hex: &'static str, 
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+// 面向 C 过程式
+
+fn new(name: &'static str, hex: &'static str) -> ColorClassicStruct {
+    ColorClassicStruct{name, hex}
+}
+
+// --- 重构 ： 面向对象风格
+
+impl ColorClassicStruct{
+    fn new(name: &'static str, hex: &'static str) -> Self{
+        Self{name, hex }
+    }
+
+    fn other(){
+        // ..
+    }
+}
+
+struct ColorTupleStruct<'a>(&'a str, &'a str);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -19,7 +39,7 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct::new("green","#00FF00");
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -28,7 +48,8 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct("green","#00FF00");
+        let green2 = ColorTupleStruct("green","#00FF00");
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -37,9 +58,20 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct;
+        let unit_struct2 = UnitStruct;
+
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
     }
 }
+
+
+// 思考： 
+
+// 1. 元组结构体 vs 具名结构体： https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=fba56cbe824be089d820771bc1dc0493
+
+// 2. 为什么需要单元结构体？
+
+// 3. 两个单元结构体的实例是否为同一个？
