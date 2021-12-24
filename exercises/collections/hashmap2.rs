270ff322a873c9ac1387e6539ae9759cd05b612d
blackanger <blackanger.z@gmail.com>
2021-12-25 00:36:10 +0800

    add 20211224

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,10 +12,11 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
+// 
 
 use std::collections::HashMap;
 
+// 注意这三个trait
 #[derive(Hash, PartialEq, Eq)]
 enum Fruit {
     Apple,
@@ -38,6 +39,50 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+
+        match fruit {
+            Fruit::Apple => {
+                // match *basket.get(&Fruit::Apple){
+                //     Some(v) => { }
+                //     None => 
+                // }
+
+                // if let None = *basket.get(&Fruit::Apple) {
+                //     basket.insert(Fruit::Apple, 4);
+                // }
+                // 思考： 操作符优先级，这里需要加括号
+                // https://doc.rust-lang.org/stable/reference/expressions.html#expression-precedence
+                let v = (*basket).get(&Fruit::Apple);
+                if v.is_none() {
+                    basket.insert(Fruit::Apple, 4);
+                }
+            }
+            Fruit::Banana => {
+                let v = (*basket).get(&Fruit::Banana);
+                if v.is_none() {
+                    basket.insert(Fruit::Banana, 2);
+                }
+            }
+            Fruit::Mango => {
+                let v = (*basket).get(&Fruit::Mango);
+                if v.is_none() {
+                    basket.insert(Fruit::Mango, 2);
+                }
+            }
+            Fruit::Lychee => {
+                let v = (*basket).get(&Fruit::Lychee);
+                if v.is_none() {
+                    basket.insert(Fruit::Lychee, 5);
+                }
+            }
+            Fruit::Pineapple => {
+                let v = (*basket).get(&Fruit::Pineapple);
+                if v.is_none() {
+                    basket.insert(Fruit::Pineapple, 2);
+                }
+            }
+        }
+
     }
 }
 
@@ -46,6 +91,7 @@ mod tests {
     use super::*;
 
     fn get_fruit_basket() -> HashMap<Fruit, u32> {
+        // turbofish ::<> 
         let mut basket = HashMap::<Fruit, u32>::new();
         basket.insert(Fruit::Apple, 4);
         basket.insert(Fruit::Mango, 2);
@@ -54,11 +100,13 @@ mod tests {
         basket
     }
 
+    // 思考：unwrap() 能不能随便用？
     #[test]
     fn test_given_fruits_are_not_modified() {
         let mut basket = get_fruit_basket();
         fruit_basket(&mut basket);
-        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
+        // 思维：为什么这里不加括号？*basket
+        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4); 
         assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
         assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
     }
